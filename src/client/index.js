d413975b5c9767e429a5b67f2f199ee7cf41a83d
Andrew Chen <hello@an.drew.sg>
2020-06-25 21:56:07 +0800

    Added button active state and re-fixed image

--- src/client/index.js
+++ src/client/index.js
@@ -2,13 +2,14 @@ import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
 import { generateHeadline } from './js/generateHeadline';
 import "./styles/style.scss";
+import img from './img/illustration.svg';
 // import "./styles/base.scss";
 // import "./styles/footer.scss";
 // import "./styles/form.scss";
 // import "./styles/header.scss";
 // import "./styles/resets.scss";
 
-export { checkForName, handleSubmit, generateHeadline }
+export { checkForName, handleSubmit, generateHeadline, img }
 
 // alert("I EXIST")
 console.log("Load successful");
