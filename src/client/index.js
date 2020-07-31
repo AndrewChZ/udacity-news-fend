758225f4bce5f3d5142e176c61d43589628bbf9b
Andrew Chen <hello@an.drew.sg>
2020-08-01 00:06:32 +0800

    added in offline service workers

--- src/client/index.js
+++ src/client/index.js
@@ -9,3 +9,13 @@ import img from './img/illustration.svg';
 export { handleSubmit, generateHeadline, inputValidate, img }
 
 console.log("Load successful");
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+     }).catch(registrationError => {
+       console.log('SW registration failed: ', registrationError);
+     });
+   });
+ }
