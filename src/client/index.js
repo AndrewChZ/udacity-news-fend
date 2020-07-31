076e755ce98920a9b5858bed1f43eb348b60f822
Andrew Chen <hello@an.drew.sg>
2020-07-31 22:24:27 +0800

    made both prod and dev script work again

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
 import { generateHeadline } from './js/generateHeadline';
 import { inputValidate } from './js/inputValidate';
+// import { sum } from './js/testFunction';
 import "./styles/style.scss";
 import img from './img/illustration.svg';
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,14 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
+    mode: 'production',
+    output: {
+        libraryTarget: "var",
+        library: "Client",
+    },
     module: {
         rules: [
             {
@@ -25,30 +26,21 @@ module.exports = {
                   // Compiles Sass to CSS
                   "sass-loader",
                 ],
-              },
-              {
-                  test: /\.(png|jpe?g|gif|svg)$/i,
-                  loader: 'file-loader',
-                  options: {
-                      outputPath: 'img',
-                      name: 'illustration.svg',
-                  },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'img',
+                    name: 'illustration.svg',
                 },
+              },
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
         })
     ]
 }
